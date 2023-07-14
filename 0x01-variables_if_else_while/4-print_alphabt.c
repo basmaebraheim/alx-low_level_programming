742668a573ea5746995c8dd83611d4f2210f05da
basma ebraheim <basmaebraheim@gmail.com>
2023-07-14 15:16:53 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
- * description - Prints alphabet in lowercase except for q and e.
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
  * Return: Always 0.
  */
 int main(void)
