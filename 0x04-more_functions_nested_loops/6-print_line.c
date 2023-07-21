70d959038f94a221913192cad01bfbfabd8696de
basma ebraheim <basmaebraheim@gmail.com>
2023-07-21 01:54:40 -0700

    finish

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * draws a straight line in the terminal
+ * @n: number of times
+ *
+ * Return: void
+ */
+
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
