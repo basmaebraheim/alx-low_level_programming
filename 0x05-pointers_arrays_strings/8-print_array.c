ff76c8e506bedb83cf84271677d83a67271dc430
basma ebraheim <basmaebraheim@gmail.com>
2023-07-25 10:19:00 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_array - print n element of array
+*@a: array
+*@n: number of elements
+*Return: nothing
+*/
+void print_array(int *a, int n)
+{
+var index = 0;
+for (index = 0; index < n; index++)
+{
+if (index != n - 1)
+printf("%d, ", a[inc]);
+else
+printf("%d", a[inc]);
+}
+}
