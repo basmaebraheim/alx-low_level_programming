e90b1ace1ea137a89cdb9b446dad0e222e965b3c
basma ebraheim <basmaebraheim@gmail.com>
2023-08-01 21:41:28 +0300

    strch

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ *
+ * @n: number of bytes
+ *
+ * @dest: destination
+ *
+ * @src: source
+ *
+ * Return: a pointer to the memory area @s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    int i = 0;
+    while(i < n)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+
+    return (dest);
+}
