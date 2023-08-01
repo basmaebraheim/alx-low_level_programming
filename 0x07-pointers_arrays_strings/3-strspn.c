707c4b751c73231505820f96267f5c33cc36e2f7
basma ebraheim <basmaebraheim@gmail.com>
2023-08-01 22:52:58 +0300

    pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,15 +13,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned i = 0;
-    while(*s && *accept)
-    {
-        if (*s != *accept) break;
+	int z = 0, x, y;
 
-        i++;
-        *s++;
-        *accept++;
-    }
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] != 32)
+		{
+			for (y = 0; accept[y] != '\0'; y++)
+			{
+				if (s[x] == accept[y])
+					z++;
+			}
+		}
+		else
+			return (z);
+	}
+		return (z);
 
-    return (i);
 }
