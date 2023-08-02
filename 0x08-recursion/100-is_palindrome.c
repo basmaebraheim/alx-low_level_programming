327f8459ea94a38546903efe8d5b15df31938d7d
basma ebraheim <basmaebraheim@gmail.com>
2023-08-02 16:23:38 +0300

    recursion tasks

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,7 +6,8 @@
 int getLength(char *s)
 {
 int length;
-for (length = 0; s[length]; length++);
+for (length = 0; s[length]; length++)
+{}
 return (length);
 }
 /**
@@ -14,15 +15,21 @@ return (length);
  * @s: string to be checked
  * Return: 1 if a char is a equal and 0 if not
  */
-int checkEqualChar(char *s,int first ,int second)
+int checkEqualChar(char *s, int first, int second)
 {
 
-if (first == second) return (1);
-if (first == second + 1 && s[first] == s[second]) return (1);
+if (first == second)
+{
+return (1);
+}
+if (first == second + 1 && s[first] == s[second])
+{
+return (1);
+}
 
 if (s[first] != s[second]) return (0);
 
-if (s[first] == s[second]) checkEqualChar(s, first + 1 , second - 1);
+if (s[first] == s[second]) checkEqualChar(s, first + 1, second - 1);
 }
 /**
  * is_palindrome - check if string palindrome
@@ -32,5 +39,5 @@ if (s[first] == s[second]) checkEqualChar(s, first + 1 , second - 1);
 int is_palindrome(char *s)
 {
 int length = getLength(s);
-return (checkEqualChar(s,0 , length - 1));
+return (checkEqualChar(s, 0, length - 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,10 +7,16 @@
 int is_prime_number_ceck(int n, int i)
 {
 
-if (i >= n) return (1);
-if (n % i == 0) return (0);
+if (i >= n)
+{
+return (1);
+}
+if (n % i == 0)
+{
+return (0);
+}
 
-return (is_prime_number_ceck(n , i + 1));
+return (is_prime_number_ceck(n, i + 1));
 }
 /**
  * is_prime_number - check if number is prime
@@ -19,6 +25,9 @@ return (is_prime_number_ceck(n , i + 1));
  */
 int is_prime_number(int n)
 {
-if (n < 2) return (0);
-return (is_prime_number_ceck(n , 2));
+if (n < 2)
+{
+return (0);
+}
+return (is_prime_number_ceck(n, 2));
 }
