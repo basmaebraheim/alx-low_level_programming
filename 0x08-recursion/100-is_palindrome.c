1046e94368311a6921c40f38ef65f37e2744e296
basma ebraheim <basmaebraheim@gmail.com>
2023-08-02 16:09:37 +0300

    recursion tasks

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,3 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * getLength - get string length
  * @s: string
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,3 @@
-
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * wildcmp - check identical strings
  * @s1: string
