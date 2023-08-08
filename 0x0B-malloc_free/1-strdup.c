48154325c83fc19a569c747d371ab91b4e191aa3
basma ebraheim <basmaebraheim@gmail.com>
2023-08-08 22:46:01 +0300

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -22,11 +22,13 @@ if (!*strCopy)
 {
 return (NULL);
 }
+
 while (i < length)
 {
 strCopy[i] = str[i];
 i++;
 }
+
 return (strCopy);
 }
 
@@ -38,10 +40,25 @@ return (strCopy);
  */
 int getStringLength(char *str)
 {
+
 int i = 0;
 while (str[i])
 {
 i++;
 }
-return (i);
+return (i + 1);
+}
+int main(void)
+{
+    char *s;
+
+    s = _strdup("Holberton School!");
+    if (s == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    printf("%s\n", s);
+    free(s);
+    return (0);
 }
