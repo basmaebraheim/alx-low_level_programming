cd219d8a470eee5262bb324e7a990f7a6f15fc19
basma ebraheim <basmaebraheim@gmail.com>
2023-08-09 00:25:35 +0300

    malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * *alloc_grid - concatenates two strings
+ * @s1: string input
+ * @s2: string input
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+int **alloc_grid(int width, int height)
+{
+int **arr, i = 0;
+
+if (width < 1 && height < 1)
+{
+return (NULL);
+}
+arr = (int **) malloc(sizeof(int *) * height);
+if (arr == NULL)
+{
+return (NULL);
+}
+while (i < height)
+{
+arr[i] = (int *) calloc(sizeof(int) , width);
+i++;
+}
+return (arr);
+}
+
