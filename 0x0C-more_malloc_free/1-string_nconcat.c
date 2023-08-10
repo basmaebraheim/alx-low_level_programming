7b6520dc4e9d8a68cf0b0758d5e67dfca1e87444
basma ebraheim <basmaebraheim@gmail.com>
2023-08-11 01:06:19 +0300

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,9 +27,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		while (s2[y] && y <= n)
+		while (s2[y])
 		y++;
 	}
+	if (y > n)
+	    y = n;
 
 	s = malloc(sizeof(char) * (x + y + 1));
 	if (s == NULL)
@@ -43,4 +45,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	s[x + y] = '\0';
 	return (s);
+}
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *concat;
+
+    concat = string_nconcat("Best ", "School !!!", 6);
+    printf("%s\n", concat);
+    free(concat);
+    return (0);
 }
