1c57e6e56be3ef720cad312a67ed820b09e76b92
basma ebraheim <basmaebraheim@gmail.com>
2023-08-18 00:16:30 +0300

    get_op_func

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,32 +1,13 @@
-#include "function_pointers.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * int_index - a function given as a
- * parameter on each element of an array.
- *
- * @array: array of int
- * @size: size of the array
- * @cmp:  pointer to the function used to compare
- *
- * Return: void
- */
-int int_index(int *array, int size, int (*cmp)(int))
+  * get_op_func - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
+int (*get_op_func(char *s))(int, int)
 {
-int i;
-if (size <= 0)
-{
-return (-1);
-}
-if (array && cmp)
-{
-i = 0;
-while (i < size)
-{
-if (cmp(array[i]))
-{
-return (i);
-}
-i++;
-}}
-return (-1);
+	 
 }
