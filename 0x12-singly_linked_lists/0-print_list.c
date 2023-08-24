5d450c0682930929d3c4224c9a404a8366b0ccd8
basma ebraheim <basmaebraheim@gmail.com>
2023-08-25 00:11:51 +0300

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,16 +10,20 @@
  */
 size_t print_list(const list_t *h)
 {
-int i = 0;
+	int i = 0;
 
-while (h)
-{
-if (!h->str)
-printf("[0] (nil)\n");
-else
-printf("[%d] %s\n", h->len, h->str);
-i++;
-h = h->next;
-}
-return (i);
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
