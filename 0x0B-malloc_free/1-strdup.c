084b9a7b5fff5b0215f043ff0d6322e21509adba
basma ebraheim <basmaebraheim@gmail.com>
2023-10-04 22:05:58 +0300

    copy str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - copy string to new location.
+ * @str: string input to copy
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+char *newStr;
+int strLength = 0, i = 0;
+
+if (str == NULL)
+{
+return (NULL);
+}
+
+while (str[strLength])
+{
+strLength++;
+}
+
+newStr = malloc(sizeof(char) * (strLength));
+if (newStr == NULL)
+{
+return (NULL);
+}
+
+while (i < strLength)
+{
+newStr[i] = str[i];
+i++;
+}
+
+return (newStr);
+}
+
+
+
