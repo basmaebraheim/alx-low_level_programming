44a625880e8a201d1a41571349f64f2a7e16dfd9
basma ebraheim <basmaebraheim@gmail.com>
2023-10-21 00:28:17 +0300

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -12,4 +12,5 @@ void beforeMain(void) __attribute__ ((constructor));
 void beforeMain(void)
 {
 printf("You're beat! and yet, you must allow,\n");
-printf("I bore my house upon my back!\n");}
+printf("I bore my house upon my back!\n");
+}
