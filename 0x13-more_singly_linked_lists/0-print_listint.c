0b20333917657159be2eebef69a8c26d01e7de5c
basma ebraheim <basmaebraheim@gmail.com>
2023-10-23 22:20:23 +0300

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int i = 0;
+	size_t i = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		i++;
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
