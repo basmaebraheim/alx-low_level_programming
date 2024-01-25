c81d5b9083f8ef627c3e1d84d46a99d4c25c315f
basmaebraheim <basmaebraheim@gmail.com>
2024-01-25 15:16:20 -0800

    sum list data

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get the sum of all
+ * the data (n) of a dlistint_t linked list.
+ * @head: pointer to head of list
+ *
+ * Return: the sum of all the data (n) of a dlistint_t linked list.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+int sum = 0;
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
+}
