7919a8052c30ee71c740c1e94f26b9ba2420d579
basma ebraheim <basmaebraheim@gmail.com>
2024-01-26 15:39:49 +0200

    add node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,19 +9,11 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-unsigned int i = 0, length;
+unsigned int i = 0;
 dlistint_t *newNode, *lastNode;
 
-if (*h == NULL)
+if (*h == NULL && idx != 0)
 return (NULL);
-
-if (idx == 0)
-return (add_dnodeint(h, n));
-
-length = dlistint_len(*h);
-if (idx == length)
-return (add_dnodeint_end(h, n));
-
 newNode = malloc(sizeof(dlistint_t));
 if (!newNode)
 return (NULL);
@@ -29,37 +21,28 @@ newNode->n = n;
 newNode->next = NULL;
 newNode->prev = NULL;
 lastNode = *h;
+if (idx == 0)
+{
+if (*h)
+(*h)->prev = newNode;
+newNode->next = *h;
+*h = newNode;
+return (newNode);
+}
 while (lastNode->next && i < idx)
 {
 lastNode = lastNode->next;
 i++;
 }
 if (i != idx)
-return (add_dnodeint_end(h, n));
-
+{
+free(newNode);
+return (NULL);
+}
 if (lastNode->prev)
 lastNode->prev->next = newNode;
 newNode->next = lastNode;
 newNode->prev = lastNode->prev;
 lastNode->prev = newNode;
 return (newNode);
-}
-/**
- * dlistint_len - get the number of elements in a linked dlistint_t list
- * @h: pointer to head of list
- *
- * Return: the number of nodes
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-size_t count = 0;
-
-if (!h)
-return (0);
-while (h)
-{
-h = h->next;
-count++;
-}
-return (count);
 }
