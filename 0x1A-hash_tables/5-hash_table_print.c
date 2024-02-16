6333862256c9784cbe4e55e4129a5be6b210af99
basma ebraheim <basmaebraheim@gmail.com>
2024-02-16 23:11:00 +0200

    hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -18,12 +18,14 @@ printf("{");
 for (i = 0; i < ht->size; i++)
 {
 temp = ht->array[i];
+while (temp)
+{
 if (flag == 1)
 printf(", ");
 printf("'%s': '%s'", temp->key, temp->value);
 flag = 1;
 temp = temp->next;
-
+}
 }
 printf("}\n");
 }
