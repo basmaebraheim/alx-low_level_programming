b11e515a9c2a06bc009a635b03599595a6977996
basma ebraheim <basmaebraheim@gmail.com>
2024-02-16 22:57:23 +0200

    hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - create hash node.
+ * @ht: the hash table you want to print
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long i;
+hash_node_t *temp;
+
+if (!ht || !ht->array)
+return;
+
+printf("{");
+for (i = 0;i < ht->size; i++)
+{
+temp = ht->array[i];
+
+if (i > 0)
+printf(", ");
+
+printf("'%s': '%s'", temp->key, temp->value);
+temp = temp->next;
+}
+printf("}");
+}
