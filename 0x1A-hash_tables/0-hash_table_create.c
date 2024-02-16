560ea7005ce54acafb6f41a34a51aa6618875530
basma ebraheim <basmaebraheim@gmail.com>
2024-02-16 21:16:42 +0200

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,4 +7,18 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+hash_table_t *table = NULL;
+table = malloc(sizeof(hash_table_t));
+
+if(!table)
+return (NULL);
+
+table->size = size;
+
+table->array = malloc(sizeof(hash_node_t));
+if(!table->array)
+return (NULL);
+
+return (table);
+
 }
