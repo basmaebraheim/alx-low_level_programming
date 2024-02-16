b3e1b3dcc2b25d43a2556f47436c38802ca9602c
basma ebraheim <basmaebraheim@gmail.com>
2024-02-16 23:04:10 +0200

    hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,6 +10,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 unsigned long i;
 hash_node_t *temp;
+int flag = 0;
 
 if (!ht || !ht->array)
 return;
@@ -18,12 +19,14 @@ printf("{");
 for (i = 0; i < ht->size; i++)
 {
 temp = ht->array[i];
-
-if (i > 0)
+while (temp != NULL)
+{
+if (flag == 1)
 printf(", ");
-
 printf("'%s': '%s'", temp->key, temp->value);
+flag = 1;
 temp = temp->next;
+}
 }
 printf("}");
 }
