b336d45e9813721c328128281af8957adb0f2035
basma ebraheim <basmaebraheim@gmail.com>
2024-02-16 21:43:26 +0200

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 /**
  * hash_table_create: creates a hash table
- * size: the size of the array
+ * @size: the size of the array
  *
  * Return: a pointer to the newly created hash table or NULL
  */
