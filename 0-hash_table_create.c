e0df33ee30828905c46951764d7b8008d220b12c
basmaebraheim <basmaebraheim@gmail.com>
2024-02-16 11:07:29 -0800

    hash tables

---
+++ 0-hash_table_create.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * hash_table_create: creates a hash table
+ * size: the size of the array
+ *
+ * Return: a pointer to the newly created hash table or NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+}
